f4ea5cabdc47070d972bc467f60fa090641c9dc4
Brandy Kuhnhenn <BKuhnhenn@student.fullsail.edu>
2020-10-06 17:20:55 -0400

    Added in the base activities for the Main Menu, Recipe Form, and Search Fail, while deleting the standard activity that came with the project.

---
+++ app/src/main/java/com/example/the_cookbook_keeper/search_fail.java
@@ -0,0 +1,14 @@
+package com.example.the_cookbook_keeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class search_fail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_fail);
+    }
+}
