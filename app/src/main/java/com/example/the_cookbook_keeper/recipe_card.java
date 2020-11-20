c50b05c0e3cca98ebf899193b879fc911c47c740
Brandy Kuhnhenn <BKuhnhenn@student.fullsail.edu>
2020-11-20 14:19:50 -0500

    Changed the image file for "sp_recipe_link_field". Also added full functionality to search_pass activity.

--- app/src/main/java/com/example/the_cookbook_keeper/recipe_card.java
+++ app/src/main/java/com/example/the_cookbook_keeper/recipe_card.java
@@ -2,7 +2,9 @@ package com.example.the_cookbook_keeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class recipe_card extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class recipe_card extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_card);
+    }
+
+    public void gotoMainMenu (View view){
+        Intent intent = new Intent(this, main_menu.class);
+        intent.putExtra("recipeSaved", true);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/the_cookbook_keeper/search_pass.java
+++ app/src/main/java/com/example/the_cookbook_keeper/search_pass.java
@@ -2,7 +2,9 @@ package com.example.the_cookbook_keeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class search_pass extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class search_pass extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_pass);
+    }
+
+    public void gotoRecipeCard (View view){
+        Intent recipeCard = new Intent(this, recipe_card.class);
+        startActivity(recipeCard);
+    }
+
+    public void gotoMainMenu (View view){
+        Intent intent = new Intent(this, main_menu.class);
+        intent.putExtra("recipeSaved", true);
+        startActivity(intent);
     }
 }
